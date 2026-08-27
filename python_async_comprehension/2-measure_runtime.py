9849cb30492539e4e84dd38a763c2d7d4e51d3f9
abdularham2341 <abdulrhman3334@gmail.com>
2026-08-28 02:57:12 +0300

    Fix async comprehension runtime

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,9 +9,11 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """Measure the runtime of four parallel async comprehensions."""
-    start_time = time.time()
+    start = time.time()
     await asyncio.gather(
-        *[async_comprehension() for i in range(4)]
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
     )
-    end_time = time.time()
-    return end_time - start_time
+    return time.time() - start
