8230f9251ac3892420073e006ceb2ed4ef5d77b8
abdularham2341 <abdulrhman3334@gmail.com>
2026-08-28 03:03:21 +0300

    Fix parallel async runtime implementation

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
-"""Measure the runtime of parallel async comprehensions."""
+"""Module that measures the runtime of async comprehensions."""
 
+from time import time
 import asyncio
-import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """Measure the runtime of four parallel async comprehensions."""
-    start = time.perf_counter()
-
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-
-    end = time.perf_counter()
+    start = time()
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    end = time()
     return end - start
