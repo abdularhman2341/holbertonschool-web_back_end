8b093be35a412eeb20872c54714e902f414e1565
abdularham2341 <abdulrhman3334@gmail.com>
2026-09-09 19:20:59 +0300

    Fix file newline requirement

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module for simple pagination."""
+"""Simple pagination module."""
 
 import csv
 import math
@@ -14,16 +14,16 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initialize the dataset cache."""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Return the cached dataset."""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -34,9 +34,9 @@ class Server:
 
     def get_page(self, page: int = 1,
                  page_size: int = 10) -> List[List]:
-        """Return a page from the dataset."""
-        assert isinstance(page, int) and isinstance(page_size, int)
-        assert page > 0 and page_size > 0
+        """Return the appropriate page of the dataset."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start_index, end_index = index_range(page, page_size)
         return self.dataset()[start_index:end_index]
